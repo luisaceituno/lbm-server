ef691010cce71a4ac6ca6271c39a7c16d40378cf
Nils Merlin Ullmann <merlinullmann@gmail.com>
2017-06-30 21:23:15 +0200

    db change

--- database.py
+++ database.py
@@ -1,19 +1,25 @@
 from pymongo import MongoClient
 from datetime import datetime
 
-client = MongoClient("mongodb://localhost:27017")
-db = client.admin
+class MongoDB():
+    client = MongoClient("mongodb://localhost:27017")
+    db = client.admin
 
-def insert_vote():
+    def __init__(self):
+        pass
 
-    vote = {"song_id": "214123",
-            "url": "test",
-            "location": {"lon": 24.22, "lat": 33.3}}
-    
-    created_time = datetime.now()
-    vote['created_time'] = created_time
-           
-    db.vote.insert_one(vote)
+    def insert_vote(self):
+
+        vote = {"song_id": "214123",
+                "url": "test",
+                "location": {"lon": 24.22, "lat": 33.3}}
+        
+        created_time = datetime.now()
+        vote['created_time'] = created_time
+            
+        self.db.vote.insert_one(vote)
 
 
-insert_vote()
+
+db = MongoDB()
+db.insert_vote()
