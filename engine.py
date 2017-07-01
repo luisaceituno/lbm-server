a1a59d98b4a0dd5b306159979f505f57bdeb41a7
Pol Zeimet <pol.zeimet@gmail.com>
2017-07-01 04:23:01 +0200

    Implemented some Methods
    >>Engine Init
    >>get_top_songlist
    -->probably not working as intended, needs testing

--- engine.py
+++ engine.py
@@ -1,17 +1,25 @@
 import pandas as pd
-
+from database import MongoDB
+import json
+from flask import jsonify
+from bson import json_util
 
 class Engine():
 
     def __init__(self):
-        pass
+        self.db = MongoDb()
 
     def get_top_songlist(self, lon, lat):
-  
-
+        
+        votes= self.db.get_votes_by_loc(lon,lat,1)
+        aggrVotes = aggregateVotes(votes)
+        top_dict = get_top_ratings(aggrVotes)
+        del top_dict['url', 'location', 'created_time']
+        top_list = json.dumps(top_dict)
+        locationsongs = { "location": { "lon": lon, "lat": lat }, "songs": top_list}
         
 
-        return "test"
+        return locationsongs
 
     def aggregateVotes(self, voteList):
         aggregatedVoteList = [voteList[0]]
